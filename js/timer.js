6a92dc113ad87697f7c740ffef5c6c83ea9984a6
Ryan Turner <RyanTurner02@protonmail.com>
2022-07-08 00:01:22 -0400

    Add functionality for the control buttons

--- js/timer.js
+++ js/timer.js
@@ -1,23 +1,33 @@
 
-document.getElementById("pomodoro-button").onclick = function () { changeTime(1500) };
-document.getElementById("short-break-button").onclick = function () { changeTime(300) };
-document.getElementById("long-break-button").onclick = function () { changeTime(600) };
+document.getElementById("pomodoro-button").onclick = function () { changeTime(1500) }; // 25 minutes
+document.getElementById("short-break-button").onclick = function () { changeTime(300) }; // 5 minutes
+document.getElementById("long-break-button").onclick = function () { changeTime(600) }; // 10 minutes
 
 document.getElementById("start-button").onclick = function () { startButtonClicked() };
 document.getElementById("stop-button").onclick = function () { stopButtonClicked() };
 document.getElementById("reset-button").onclick = function () { resetButtonClicked() };
 
+let initialSeconds = 1500; // 25 minutes
 let seconds = 1500; // 25 minutes
-updateTimerText(getConvertedTime(seconds));
+let timer;
 
-let timer = setInterval(countDown, 1000);
+function startButtonClicked() {
+    timer = setInterval(countDown, 1000);
+}
 
-function startButtonClicked() { }
-function stopButtonClicked() { }
-function resetButtonClicked() { }
+function stopButtonClicked() {
+    clearInterval(timer);
+}
+
+function resetButtonClicked() {
+    seconds = initialSeconds;
+    updateTimerText(getConvertedTime(seconds));
+    clearInterval(timer);
+}
 
 function changeTime(newSeconds) {
     updateTimerText(getConvertedTime(newSeconds));
+    initialSeconds = newSeconds;
     seconds = newSeconds;
 }
 
