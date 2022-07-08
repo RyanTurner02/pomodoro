273a3644769d98cea77de11849fb84d19d9adae4
Ryan Turner <RyanTurner02@protonmail.com>
2022-07-07 23:48:32 -0400

    Change starting time on button clicks

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,25 @@
 
+document.getElementById("pomodoro-button").onclick = function () { changeTime(1500) };
+document.getElementById("short-break-button").onclick = function () { changeTime(300) };
+document.getElementById("long-break-button").onclick = function () { changeTime(600) };
+
+document.getElementById("start-button").onclick = function () { startButtonClicked() };
+document.getElementById("stop-button").onclick = function () { stopButtonClicked() };
+document.getElementById("reset-button").onclick = function () { resetButtonClicked() };
+
 let seconds = 1500; // 25 minutes
 updateTimerText(getConvertedTime(seconds));
 
 let timer = setInterval(countDown, 1000);
+
+function startButtonClicked() { }
+function stopButtonClicked() { }
+function resetButtonClicked() { }
+
+function changeTime(newSeconds) {
+    updateTimerText(getConvertedTime(newSeconds));
+    seconds = newSeconds;
+}
 
 function countDown() {
     if (seconds-- <= 0) {
@@ -16,7 +33,7 @@ function getConvertedTime(totalSeconds) {
     let minutes = Math.floor(totalSeconds / 60);
     let seconds = totalSeconds % 60;
 
-    if(seconds < 10) {
+    if (seconds < 10) {
         return `${minutes}:0${seconds}`;
     }
     return `${minutes}:${seconds}`;
